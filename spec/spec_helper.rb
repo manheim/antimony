943dc5fb93a02d4847a14c2875989c16da85360b
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2015-04-27 14:45:45 -0400

    Configure tests to run in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
+  config.order = :random
+
   # Output
   config.color_enabled = true
   config.tty = true
